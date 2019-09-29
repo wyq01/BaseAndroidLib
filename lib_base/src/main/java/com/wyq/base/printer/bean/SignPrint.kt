59fdb36c396e226f0f14e6fcd6833d6b99e6616e
wyq_10 <wyq_10@126.com>
2019-09-29 15:21:47 +0800

    打印实体添加部分方法

--- lib_base/src/main/java/com/wyq/base/printer/bean/SignPrint.kt
+++ lib_base/src/main/java/com/wyq/base/printer/bean/SignPrint.kt
@@ -5,13 +5,38 @@ package com.wyq.base.printer.bean
  */
 class SignPrint(var signTip: String?, var signPath: String?) : BasePrint(BasePrint.Type.SIGNATURE.value) {
     constructor(imgPath: String?): this(null, imgPath)
-    var textSize: Int = BasePrint.TextSize.X24.value // 字体大小，默认为24
-    var underLine: Boolean = false // 是否带下划线
-    var bold: Boolean = false // 是否加粗
-    var showSignTip: Boolean = true // 是否打印提示
+    private var textSize: Int = BasePrint.TextSize.X24.value // 字体大小，默认为24
+    private var underLine: Boolean = false // 是否带下划线
+    private var bold: Boolean = false // 是否加粗
+    private var showSignTip: Boolean = true // 是否打印提示
 
     public fun textSize(textSize: Int): SignPrint {
         this.textSize = textSize
+        return this
+    }
+
+    public fun textSizeX16(): SignPrint {
+        this.textSize = BasePrint.TextSize.X16.value
+        return this
+    }
+
+    public fun textSizeX24(): SignPrint {
+        this.textSize = BasePrint.TextSize.X24.value
+        return this
+    }
+
+    public fun textSizeX32(): SignPrint {
+        this.textSize = BasePrint.TextSize.X32.value
+        return this
+    }
+
+    public fun textSizeX48(): SignPrint {
+        this.textSize = BasePrint.TextSize.X48.value
+        return this
+    }
+
+    public fun textSizeX64(): SignPrint {
+        this.textSize = BasePrint.TextSize.X64.value
         return this
     }
 
--- lib_base/src/main/java/com/wyq/base/printer/bean/TextPrint.kt
+++ lib_base/src/main/java/com/wyq/base/printer/bean/TextPrint.kt
@@ -5,13 +5,38 @@ package com.wyq.base.printer.bean
  * 文本
  */
 class TextPrint(var text: String?) : BasePrint(BasePrint.Type.TEXT.value) {
-    var textSize: Int = BasePrint.TextSize.X24.value // 字体大小，默认为24
-    var underLine: Boolean = false // 是否带下划线
-    var bold: Boolean = false // 是否加粗
-    var align: String = BasePrint.Align.LEFT.value // 对齐方式
+    private var textSize: Int = BasePrint.TextSize.X24.value // 字体大小，默认为24
+    private var underLine: Boolean = false // 是否带下划线
+    private var bold: Boolean = false // 是否加粗
+    private var align: String = BasePrint.Align.LEFT.value // 对齐方式
 
     public fun textSize(textSize: Int): TextPrint {
         this.textSize = textSize
+        return this
+    }
+
+    public fun textSizeX16(): TextPrint {
+        this.textSize = BasePrint.TextSize.X16.value
+        return this
+    }
+
+    public fun textSizeX24(): TextPrint {
+        this.textSize = BasePrint.TextSize.X24.value
+        return this
+    }
+
+    public fun textSizeX32(): TextPrint {
+        this.textSize = BasePrint.TextSize.X32.value
+        return this
+    }
+
+    public fun textSizeX48(): TextPrint {
+        this.textSize = BasePrint.TextSize.X48.value
+        return this
+    }
+
+    public fun textSizeX64(): TextPrint {
+        this.textSize = BasePrint.TextSize.X64.value
         return this
     }
 
@@ -27,6 +52,21 @@ class TextPrint(var text: String?) : BasePrint(BasePrint.Type.TEXT.value) {
 
     public fun align(align: String): TextPrint {
         this.align = align
+        return this
+    }
+
+    public fun alignCenter(): TextPrint {
+        this.align = BasePrint.Align.CENTER.value
+        return this
+    }
+
+    public fun alignLeft(): TextPrint {
+        this.align = BasePrint.Align.LEFT.value
+        return this
+    }
+
+    public fun alignRight(): TextPrint {
+        this.align = BasePrint.Align.RIGHT.value
         return this
     }
 
